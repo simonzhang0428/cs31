0dc8b51da7eae4b7b5894b2c7f438e6eabb94a03
simon <simonzhang0428@gmail.com>
2020-08-10 19:29:47 -0700

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by Jing Ping on 2020/8/10.
+//  Copyright © 2020 Simon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
