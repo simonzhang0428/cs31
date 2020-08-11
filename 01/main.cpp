6a108e3f2e5a974d91f93827f6e4d4f4ae32bdf7
simon <simonzhang0428@gmail.com>
2020-08-11 15:10:40 -0700

    original code

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  01
-//
-//  Created by Jing Ping on 2020/8/11.
-//  Copyright © 2020 Simon. All rights reserved.
-//
+// Code for Project 1
+// Report poll results
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    int numberSurveyed;
+    int obey;
+    int disobey;
+
+    cout << "How many people were surveyed? ";
+    cin >> numberSurveyed;
+    cout << "How many of them are obeying the stay-at-home and other health orders? ";
+    cin >> obey;
+    cout << "How many of them are disobeying the stay-at-home and other health orders? ";
+    cin >> disobey;
+
+    double pctObey = 100.0 * obey / numberSurveyed;
+    double pctDisobey = 100.0 * disobey / numberSurveyed;
+
+    cout.setf(ios::fixed);
+    cout.precision(1);
+
+    cout << endl;
+    cout << pctObey << "% say they are obeying the orders." << endl;
+    cout << pctDisobey << "% say they are disobeying the orders." << endl;
+
+    if (obey > disobey)
+        cout << "More people are obeying than disobeying the orders." << endl;
+    else
+        cout << "More people are disobeying than obeying the orders." << endl;
 }
