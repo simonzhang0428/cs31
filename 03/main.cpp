35afd0a35b2fac921cb0dfac8e4c8c9d7ae08cb6
simon <simonzhang0428@gmail.com>
2020-08-12 11:32:17 -0700

    assert

--- 03/main.cpp
+++ 03/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+#include <cassert>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    assert(3 > 2);
+    cout << "Hello, World!\n";
     return 0;
 }
